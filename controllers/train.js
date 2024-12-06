66043f5068699b0f7cbefbf47858f19309c47c12
KRATIK_BOHRA <kratikbohra5@gmail.com>
2024-12-06 19:45:42 +0530

    added updateseats function

--- controllers/train.js
+++ controllers/train.js
@@ -54,7 +54,28 @@ const getTrains = async (req, res) => {
   res.status(200).json({ message: trains });
 };
 
+const updateSeats = async (req, res) => {
+  const { train_num, availableSeats } = req.body;
+  const token = req.header("Authorization")?.split(" ")[1];
+  if (!token)
+    return res
+      .status(401)
+      .json({ message: "No token provided; access denied" });
+  const decrypted = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  if (!decrypted) return res.status(401).json({ message: "Invalid Token" });
+  const train = await Train.findOne({ where: { train_num: train_num } });
+  if (!train) {
+    return res
+      .status(401)
+      .json({ message: `Train with number ${train_num} not found` });
+  }
+  train.avl_seats = availableSeats;
+  train.save();
+  res.status(200).json({ message: "Seats updates successfully" });
+};
+
 module.exports = {
   addTrain,
   getTrains,
+  updateSeats,
 };
