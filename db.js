084c26b6d14dc762f5ef35164b6b6a4dd9da19aa
KRATIK_BOHRA <kratikbohra5@gmail.com>
2024-12-06 15:20:10 +0530

    using sequelize as orm tool

--- db.js
+++ db.js
@@ -1,12 +1,26 @@
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 
+// require("dotenv").config();
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: Number(process.env.DB_PORT),
+// });
+
+// module.exports = pool;
+const { Sequelize } = require("sequelize");
 require("dotenv").config();
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
-});
 
-module.exports = pool;
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+  }
+);
+module.exports = sequelize;
