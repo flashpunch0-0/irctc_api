c0a74863c2976614a3b181a893c15c39324950dd
KRATIK_BOHRA <kratikbohra5@gmail.com>
2024-12-06 13:06:30 +0530

    postgres connected

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-const crypto = require("crypto");
+const db = require("./db");
+
 require("dotenv").config();
 const app = express();
 app.use(cors());
+
 app.use(express.json());
 const PORT = process.env.PORT || 3010;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+db.query("SELECT NOW()", (err, res) => {
+  if (err) {
+    console.error("Error connecting to PostgreSQL:", err);
+    process.exit(1);
+  } else {
+    console.log("PostgreSQL connected successfully at:", res.rows[0].now);
+  }
 });
